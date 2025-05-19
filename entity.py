ff3cdacb854a297686dd4c8f2e38c0668f245f10
Logan Jordan <ljordan@cocc.edu>
2025-05-19 14:24:09 -0700

    Add ghosts setup

--- entity.py
+++ entity.py
@@ -64,7 +64,7 @@ class Entity(object):
 
     def render(self, screen):
         if self.visible:
-            p = self.position.asInt()
+            p = self.position.as_int()
             pygame.draw.circle(screen, self.color, p, self.radius)
 
     def update(self, dt):
